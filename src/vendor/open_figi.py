a0e3d00e58818142fd537ad7692287c025eb692b
Dustin Baek <kbbaek18@gmail.com>
2026-03-31 13:39:48 +0100

    add base url for open figi

--- src/vendor/open_figi.py
+++ src/vendor/open_figi.py
@@ -6,8 +6,11 @@ from .general_vendor import GeneralVendorApi
 
 
 class OpenFigiApi(GeneralVendorApi):
-    def __init__(self, base_url: str, api_key: str | None = None):
-        super().__init__(base_url, api_key)
+    BASE_URL = r"https://api.openfigi.com/v{}"
+
+    def __init__(self, version: int = 3, api_key: str | None = None):
+        self.base_url = OpenFigiApi.BASE_URL.format(version)
+        super().__init__(self.base_url, api_key)
         if self.api_key:
             self.header |= {"X-OPENFIGI-APIKEY": self.api_key}
 
